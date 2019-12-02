f6f549822405a5ecaf73aa7b476c9d56270f972e
Tyler Stewart <hello@tylerstewart.ca>
2019-12-01 20:56:40 -0700

    day 2, part 2

--- src/days/day2.rs
+++ src/days/day2.rs
@@ -21,15 +21,43 @@ fn get_duplicate_count(id: &String) -> (i32, i32) {
   dups
 }
 
+fn find_common_id(ids: &Vec<String>) -> Option<String> {
+  for i in 0..ids.len() {
+    let compare_id = &ids[i];
+
+    for current_id in &ids[(i + 1)..] {
+
+      let mut diff_index: Vec<usize> = Vec::new();
+
+      for i in 0..compare_id.len() {
+        let compare_char = compare_id.chars().nth(i).unwrap();
+        let current_char = current_id.chars().nth(i).unwrap();
+
+        if compare_char != current_char {
+          diff_index.push(i);
+          if diff_index.len() == 2 {
+            break;
+          }
+        }
+      }
+
+      if diff_index.len() == 1 {
+        let wrong_index = diff_index[0];
+        let common = format!("{}{}", &current_id[..wrong_index], &current_id[(wrong_index + 1)..]);
+        return Some(common);
+      }
+    }
+  }
+  None
+}
+
 pub fn run() {
   let ids = utils::request::get("https://adventofcode.com/2018/day/2/input").unwrap();
 
   // Part 1
   let mut duplicates = (0, 0);
-  for id in ids {
-    println!("{}", id);
+  for id in &ids {
     let (twos, threes) = get_duplicate_count(&id);
-    println!("{:#?}", duplicates);
 
     duplicates.0 += twos;
     duplicates.1 += threes;
@@ -37,4 +65,8 @@ pub fn run() {
 
   let sum = duplicates.0 * duplicates.1;
   println!("Part 1: {}", sum);
+
+  // Part 2
+  let common = find_common_id(&ids).unwrap();
+  println!("Part 2: {}", common);
 }
